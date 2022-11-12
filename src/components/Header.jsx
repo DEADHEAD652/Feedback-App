a29dfc79b0466adffa3f7a7ef81854701452b5a9
DEADHEAD652 <hamzajjmalik@gmail.com>
2022-11-12 21:51:08 +0500

    Routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header({text ,bgColor,textColor}) {
 }
 
 Header.defaultProps = {
-text:'Fuck You',bgColor: 'rgba(0,0,0,0.4)', textColor:'#ff6a95'
+text:'Feedback',bgColor: 'rgba(0,0,0,0.4)', textColor:'#ff6a95'
 
 }
 Header.prototype = {
---
+++ src/components/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from '../shared/card'
+
+function Aboutpage() {
+  return (
+    <Card>
+      <div className='about'>
+<h1>About this Project</h1>
+<p>This a react app to leave feedback for  product or service</p>
+<p>Version : 1.0.0</p>
+<p>
+    <a href="/">Back to Home</a>
+</p>
+      </div>
+    </Card>
+  )
+}
+
+export default Aboutpage
