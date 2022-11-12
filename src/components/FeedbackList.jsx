b4d941bf7384e92151d8180e0f28b6b6626bc039
DEADHEAD652 <hamzajjmalik@gmail.com>
2022-11-12 17:13:23 +0500

    fading animatiom

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion ,AnimatePresence } from "framer-motion"
 import PropTypes from 'prop-types'
 import Feedbackitem from './Feedbackitem'
 
@@ -6,15 +7,29 @@ function FeedbackList({feedback , handleDelete}) {
   if(!feedback || feedback.length ===0){
     return <p>No feedback yet!</p>
   }
-    return (
 
-    <div className='feedback-list'>
+  return (
+
+    <div className='feedback-list'> <AnimatePresence>
       {feedback.map((item) =>(
+<motion.div key={item.id}  initial={{opacity:0}} animate={{opacity:1}}  exit={{opacity:0}}     >
+
 <Feedbackitem key={item.id} item={item} handleDelete = {handleDelete}/>
 
+</motion.div>
 ))}
+</AnimatePresence>
     </div>
   )
+//     return (
+
+//     <div className='feedback-list'>
+//       {feedback.map((item) =>(
+// <Feedbackitem key={item.id} item={item} handleDelete = {handleDelete}/>
+
+// ))}
+//     </div>
+//   )
 }
 
 FeedbackList.propTypes = {
